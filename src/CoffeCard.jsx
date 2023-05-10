b7db0b8ceb07717bd7de65aae2e1c1ad0b23fe90
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-10 10:01:42 +0600

    confirm alert when delete a product "alert by sweetalert2" -> fetch dynamically with product id -> successfully delete product from database

--- src/CoffeCard.jsx
+++ src/CoffeCard.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import Swal from "sweetalert2";
 
 const CoffeCard = ({ coffee }) => {
-  const { name, quantity, supplier, taste, category, details, photo } = coffee;
+  const { _id, name, quantity, supplier, taste, photo } = coffee;
+
+  const handleDelete = (_id) => {
+    // console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // console.log("Delete Confirm!");
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+            }
+          });
+      }
+    });
+  };
   return (
-    <div className="m-2 p-2">
-      <div className="card card-side bg-base-100 shadow-xl">
+    <div className="m-2 p-2 border rounded-2xl">
+      <div className="card card-side bg-base-100 shadow">
         <figure>
           <img src={photo} alt="Coffee" />
         </figure>
@@ -28,7 +56,12 @@ const CoffeCard = ({ coffee }) => {
             <div className="btn-group btn-group-vertical space-y-4">
               <button className="btn">view</button>
               <button className="btn">edit</button>
-              <button className="btn">X</button>
+              <button
+                onClick={() => handleDelete(_id)}
+                className="btn bg-orange-600"
+              >
+                X
+              </button>
             </div>
           </div>
         </div>
